ef94ff3817f96f267a7ff94846e5c333a02db7e2
MJ Kim <myoungjunkim@172.20.nate.com>
2014-08-31 11:09:05 +0900

    Initial Commit

---
+++ HelloTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloTest
+//
+//  Created by MJ Kim on 2014. 8. 31..
+//  Copyright (c) 2014년 HTML5 Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
